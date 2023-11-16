fd15c141c1b7e19ab15c2f253db54e1d9d80485e
Weslley <weslley_pablo@hotmail.com.br>
2023-11-16 20:26:18 -0300

    continuação do commit anterior

--- components/brand/brand.js
+++ components/brand/brand.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import styles from './brand.module.css';
 
-export default function Brand({ name }) {
+export default function Brand({ topics }) {
   const formatString = (str) => {
     return str[0].toUpperCase() + str.slice(1);
   };
 
   const compareSkills = (a, b) => {
     const order = ["full-stack", "back-end", "front-end"];
-  
-    // Se a estiver no array de ordem, coloca a antes de b
     if (order.includes(a)) return -1;
-    // Se b estiver no array de ordem, coloca b antes de a
     if (order.includes(b)) return 1;
-    // Mantém a ordem original para outros itens
     return 0;
   };
 
-  if (Array.isArray(name)) {
-    // Se name for um array, renderiza uma lista de spans
+  if (Array.isArray(topics)) {
     return (
       <>
-        {name
+        {topics
           .sort(compareSkills)
           .map((item, index) => (
             <span key={index} className={styles.brand}>
@@ -32,6 +27,5 @@ export default function Brand({ name }) {
     );
   }
 
-  // Se name não for um array, renderiza apenas um <span>
-  return <span className={styles.brand}>{formatString(name)}</span>;
+  return <span className={styles.brand}>{formatString(topics)}</span>;
 }
