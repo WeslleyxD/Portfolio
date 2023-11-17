c7abff47fe2f3d45542ac559e29f7736f2f4f3ee
Weslley <weslley_pablo@hotmail.com.br>
2023-11-17 20:48:36 -0300

    fix: do commit anterior

--- components/brand/brand.js
+++ components/brand/brand.js
@@ -6,19 +6,19 @@ export default function Brand({ name }) {
     return str[0].toUpperCase() + str.slice(1);
   };
 
-  const order = {
-    "back-end": 1,
-    "full-stack": 2,
-    "front-end": 3,
-    "javascript": 4,
-    "python": 5,
-    "golang": 6,
-  };
-
-  const sortedArray = [...name].sort((a, b) => (order[a] || Number.MAX_SAFE_INTEGER) - (order[b] || Number.MAX_SAFE_INTEGER));
-
-  if (Array.isArray(sortedArray)) {
+  if (Array.isArray(name)) {
     // Se name for um array, renderiza uma lista de spans
+    const order = {
+      "back-end": 1,
+      "full-stack": 2,
+      "front-end": 3,
+      "javascript": 4,
+      "python": 5,
+      "golang": 6,
+    };
+  
+    const sortedArray = [...name].sort((a, b) => (order[a] || Number.MAX_SAFE_INTEGER) - (order[b] || Number.MAX_SAFE_INTEGER));
+
     return (
       <>
         {sortedArray
