1210646dc496f187586d34324ab0c702be68b762
weslley <weslley_pablo@hotmail.com.br>
2024-02-24 21:57:02 -0300

    correção image repo

--- components/cardProjects/cardProjects.js
+++ components/cardProjects/cardProjects.js
@@ -8,12 +8,14 @@ export default function CardProjects({ data }) {
   const [imageError, setImageError] = useState(false);
   const fallBackSrc = fallback.src
 
-  console.log(data)
   return (
     <Link href={data.html_url} target='_blank'>
       <div className={styles.container}>
         <div className={styles.card}>
-          <Image src={imageError ? fallBackSrc : `/repo/${data.id}.png`} alt="error" width="300" height="128" title={data.name} style={{objectFit:"contain"}}onError={() => setImageError(true)}/>
+          <div>
+            <Image src={imageError ? fallBackSrc : `/repo/${data.name.toLowerCase()}.png`} alt="error" width="300" height="128" title={data.name} style={{ objectFit: "contain" }} onError={() => setImageError(true)} />
+            <h3>{data.name}</h3>
+          </div>
           <div className={styles.content}><h4>{data.description}</h4></div>
         </div>
       </div >
--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -6,7 +6,6 @@ import { useEffect, useState } from 'react';
 import styles from './page.module.css';
 import Brandfilter from '../../../components/brandfilter/brandFilter';
 import CardProjects from '../../../components/cardProjects/cardProjects';
-import Pagination from '../../../components/pagination/pagination';
 
 const sortedData = (a, b) => {
     const dataA = new Date(a.created_at);
@@ -171,7 +170,7 @@ export default function Projects() {
                         <Link href="/" className={styles.return}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="mr-1 h-4 w-4 rotate-180 transition-transform group-hover:-translate-x-2" aria-hidden="true">
                                 <path fillRule="evenodd" d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z" clipRule="evenodd"></path>
-                            </svg>{' '}
+                            </svg>
                             <h2>Weslley Silva</h2>
                         </Link>
                         <h1>Todos os Projetos</h1>
